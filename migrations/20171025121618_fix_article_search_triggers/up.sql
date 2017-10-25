b90d7b9d1e2b65526ce0435f37b0f6d886c8a1a8
Magnus Hoff <maghoff@gmail.com>
2017-10-25 14:19:33 +0200

    Fix triggers for keeping search index up to date

--- migrations/20171025121618_fix_article_search_triggers/up.sql
+++ migrations/20171025121618_fix_article_search_triggers/up.sql
@@ -1,12 +1,7 @@
-CREATE VIRTUAL TABLE article_search USING fts5(
-    title,
-    body,
-    slug UNINDEXED,
-    tokenize = 'porter unicode61 remove_diacritics 1'
-);
-
-INSERT INTO article_search(title, body, slug)
-    SELECT title, body, slug FROM article_revisions WHERE latest = 1;
+DROP TRIGGER article_revisions_ai;
+DROP TRIGGER article_revisions_ad;
+DROP TRIGGER article_revisions_au_disable;
+DROP TRIGGER article_revisions_au_enable;
 
 CREATE TRIGGER article_revisions_ai AFTER INSERT ON article_revisions WHEN new.latest = 1 BEGIN
   DELETE FROM article_search WHERE rowid = new.article_id;
@@ -23,3 +18,7 @@ END;
 CREATE TRIGGER article_revisions_au_enable AFTER UPDATE ON article_revisions WHEN old.latest = 0 AND new.latest = 1 BEGIN
   INSERT INTO article_search(rowid, title, body, slug) VALUES (new.article_id, new.title, new.body, new.slug);
 END;
+
+DELETE FROM article_search;
+INSERT INTO article_search(title, body, slug)
+    SELECT title, body, slug FROM article_revisions WHERE latest = 1;
