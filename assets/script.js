2e406f4480f528829a0a849aab9d6d46ab5d94ae
Magnus Hoff <maghoff@gmail.com>
2017-11-24 17:04:36 +0100

    Handle authorization problems when saving article with JS

--- assets/script.js
+++ assets/script.js
@@ -20,6 +20,38 @@ function isEdited(form) {
     return false;
 }
 
+function loginDialog(loginUrl) {
+    const loginDialogHtml = "<div class=popup><p>Your changes could not be saved</p><p>Log in and try again</p><div><button>Never mind</button> <a href='' target=login>Open login page</a></div></div>";
+
+    const dialog = document.createElement("div");
+    dialog.className = "modal-block";
+    dialog.innerHTML = loginDialogHtml;
+
+    const loginLink = dialog.querySelector("a");
+    const dismiss = dialog.querySelector("button");
+
+    loginLink.setAttribute("href", loginUrl);
+
+    document.body.appendChild(dialog);
+    loginLink.focus();
+
+    function remove() {
+        document.body.removeChild(dialog);
+    }
+
+    return new Promise((resolve, reject) => {
+        loginLink.addEventListener("click", () => {
+            remove();
+            resolve();
+        });
+
+        dismiss.addEventListener("click", () => {
+            remove();
+            resolve();
+        });
+    });
+}
+
 let hasBeenOpen = false;
 function openEditor() {
     const container = document.querySelector(".container");
@@ -53,6 +85,7 @@ function openEditor() {
 
         const body = queryArgsFromForm(form);
         textarea.disabled = true;
+        // TODO Disable other interaction as well: title editor, cancel and OK buttons
 
         fetch(
             form.getAttribute("action"),
@@ -65,39 +98,47 @@ function openEditor() {
                 credentials: "same-origin",
             }
         ).then(response => {
+            if ((response.status === 401) && (response.headers.has("location"))) {
+                return loginDialog(response.headers.get("location"))
+                    .then(() => {
+                        textarea.disabled = false;
+                    });
+            }
+
             if (!response.ok) throw new Error("Unexpected status code (" + response.status + ")");
 
-            return response.json();
-        }).then(result => {
-            // Update url-bar, page title and footer
-            window.history.replaceState(null, result.title, result.slug == "" ? "." : result.slug);
-            document.querySelector("title").textContent = result.title;
-            lastUpdated.innerHTML = result.last_updated;
-            lastUpdated.classList.remove("missing");
+            return response.json()
+                .then(result => {
+                    // Update url-bar, page title and footer
+                    window.history.replaceState(null, result.title, result.slug == "" ? "." : result.slug);
+                    document.querySelector("title").textContent = result.title;
+                    lastUpdated.innerHTML = result.last_updated;
+                    lastUpdated.classList.remove("missing");
 
-            // Update body:
-            rendered.innerHTML = result.rendered;
+                    // Update body:
+                    rendered.innerHTML = result.rendered;
 
-            form.elements.title.value = result.title;
-            shadow.value = textarea.value = result.body;
+                    form.elements.title.value = result.title;
+                    shadow.value = textarea.value = result.body;
 
-            // Update form:
-            form.elements.base_revision.value = result.revision;
-            for (const element of form.elements) {
-                element.defaultValue = element.value;
-            }
+                    // Update form:
+                    form.elements.base_revision.value = result.revision;
+                    for (const element of form.elements) {
+                        element.defaultValue = element.value;
+                    }
 
-            if (!result.conflict) {
-                container.classList.remove('edit');
-            }
+                    if (!result.conflict) {
+                        container.classList.remove('edit');
+                    }
 
-            textarea.disabled = false;
-            autosizeTextarea(textarea, shadow);
+                    textarea.disabled = false;
+                    autosizeTextarea(textarea, shadow);
 
-            if (result.conflict) {
-                alert("Your edit came into conflict with another change and has not been saved.\n" +
-                    "Please resolve the merge conflict and save again.");
-            }
+                    if (result.conflict) {
+                        alert("Your edit came into conflict with another change and has not been saved.\n" +
+                            "Please resolve the merge conflict and save again.");
+                    }
+                });
         }).catch(err => {
             textarea.disabled = false;
             console.error(err);
